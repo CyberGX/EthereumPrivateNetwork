a92691112ded91739e68ca7d6ff0d295f97c80db
CyberGX <anonymous.aquila@gmail.com>
2018-08-05 13:05:55 +0430

    add startGeth.sh

---
+++ startGeth.sh
@@ -0,0 +1,1 @@
+geth --networkid 4224 --mine --nodiscover --rpc --rpcport "8545" --port "30303" --rpccorsdomain "*" --nat "any" --rpcapi eth,web3,personal,net --unlock 0 --password "/<your direcotry>/password.sec"  --rpcaddr 0.0.0.0
